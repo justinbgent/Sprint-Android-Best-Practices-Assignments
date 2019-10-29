ffc13e7413e0342531edbe0d52dbfa5bc7bc9518
Justin Gent <justinbgent@gmail.com>
2019-10-29 14:20:02 -0600

    Add dropBreadCrumbs function above MainActivity class, used in each button.

--- M02/Assignment/app/src/main/java/com/schoolwork/crashlytics/MainActivity.kt
+++ M02/Assignment/app/src/main/java/com/schoolwork/crashlytics/MainActivity.kt
@@ -2,7 +2,12 @@ package com.schoolwork.crashlytics
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.crashlytics.android.Crashlytics
 import kotlinx.android.synthetic.main.activity_main.*
+
+fun dropBreadCrumb(className: String, methodName: String, generalInfo: String){
+    Crashlytics.log("$className -- $methodName -- $generalInfo")
+}
 
 class MainActivity : AppCompatActivity() {
     lateinit var string: String
@@ -12,20 +17,24 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         btn_null_pointer.setOnClickListener {
+            dropBreadCrumb("MainActivity", "btn_null_pointer listener", "Unsafe null call")
             val int: Int? = null
             int!!.plus(6)
         }
 
         btn_not_initialized.setOnClickListener {
+            dropBreadCrumb("MainActivity", "btn_not_initialized listener", "Uses uninitialized lateinit")
             "Test$string"
         }
 
         btn_divide_by_0.setOnClickListener {
+            dropBreadCrumb("MainActivity", "btn_divide_by_zero listener", "Divides with 0")
             val divisionByZero = 6/0
         }
 
         btn_bad_cast.setOnClickListener {
-            var double: Double = 7.3
+            dropBreadCrumb("MainActivity", "btn_bad_cast listener", "Casts double as Int")
+            val double: Double = 7.3
             val fakeInt: Int = double as Int
         }
     }
