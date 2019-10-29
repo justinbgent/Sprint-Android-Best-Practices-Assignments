913db32fd0751337615f1e919db30f42fe1a4acb
Justin Gent <justinbgent@gmail.com>
2019-10-29 13:56:07 -0600

    Add buttons for 4 different crash types and logic in MainActivity

--- M02/Assignment/app/src/main/java/com/schoolwork/crashlytics/MainActivity.kt
+++ M02/Assignment/app/src/main/java/com/schoolwork/crashlytics/MainActivity.kt
@@ -2,11 +2,31 @@ package com.schoolwork.crashlytics
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var string: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_null_pointer.setOnClickListener {
+            val int: Int? = null
+            int!!.plus(6)
+        }
+
+        btn_not_initialized.setOnClickListener {
+            "Test$string"
+        }
+
+        btn_divide_by_0.setOnClickListener {
+            val divisionByZero = 6/0
+        }
+
+        btn_bad_cast.setOnClickListener {
+            var double: Double = 7.3
+            val fakeInt: Int = double as Int
+        }
     }
 }
