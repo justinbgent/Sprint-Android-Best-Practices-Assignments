f6855737b26e71f81eda702065c25598545985a9
Justin Gent <justinbgent@gmail.com>
2019-10-31 14:48:15 -0600

    Put dagger dependencies in gradle

--- M04/Assignment/app/src/main/java/com/example/sprintpokemonchallenge/model/PokemonIndex.kt
+++ M04/Assignment/app/src/main/java/com/example/sprintpokemonchallenge/model/PokemonIndex.kt
@@ -5,7 +5,7 @@ import com.google.gson.annotations.SerializedName
 data class PokemonDetail(val abilities: List<Ability>, val id: Int, val name: String, val sprites: Sprites, val types: List<Types>)
 //Next five data class's help flesh out the above class, See parameters
 data class Ability(val ability: AbilityDetails)
-    //Below A
+//Below A
 data class AbilityDetails(val name: String/*, val url: String*/)
 
 data class Sprites(@SerializedName("front_default")val pokemonSprite: String)
